fda79a1a002b2f5f2f8e8e92fb34e8e745d94c4c
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-17 13:16:33 +0300

    am proud of me

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,21 @@
 def write_file(file_name, file_content):
-    pass
+    file_name=(f"{file_name}.txt")
+    with open(file_name,'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name=(f"{file_name}.txt")
+    with open(file_name,'a')as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_name=(f"{file_name}.txt")
+    with open(file_name,'r')as f:
+        return f.read()
+
+write_file(file_name='logfile',file_content='Log1: Bananas are added')
+append_file(file_name='logfile',append_content='Log2:3 Bananas subtracted')
+read_file(file_name='logfile')
+content=read_file('logfile')
+print(content)
